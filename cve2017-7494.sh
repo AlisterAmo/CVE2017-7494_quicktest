a74f9fcae0decf1f30fbc99afe23d2cec86c7ad5
Alister Amo <whitehat.alister@gmail.com>
2017-05-26 20:14:02 +0200

    Update cve2017-7494.sh

--- cve2017-7494.sh
+++ cve2017-7494.sh
@@ -28,15 +28,28 @@ export sambaminor="$(echo $sambaversionstring | cut -d"." -f 2)"
 export sambarev="$(echo $sambaversionstring | cut -d"." -f3)"
 }
 
+function performupgrade {
+get_package_type
+    case OS in
+    "DEB")
+      apt-get update && apt-get install --only-upgrade samba && echo "Samba upgrade finished :)" || echo "Something went wrong! Could not automatically upgrade samba :("
+      ;;
+    "RPM")
+      yum update samba && echo "Samba upgrade finished :)" || echo "Something went wrong! Could not automatically upgrade samba :("
+      ;;
+    esac
+}
+
 function performcheck {
 # Samba versions from 3.5.0 onwards are vulnerable
 # Samba 4.6.4, 4.5.10 and 4.4.14 have been issued as security releases to correct the defect
-# major version 3 && minor version > 4  == vulnerable
-# major version 4 && minor version < 4  == vulnerable
+# major version 3 && minor version > 4  == vulnerable and no official security released
+# major version 4 && minor version < 4  == vulnerable and no official security released 
 # major version 4 && minor version = 4  && rev < 14 == vulnerable
 # major version 4 && minor version = 5  && rev < 10 == vulnerable
 # major version 4 && minor version = 6  && rev < 4  == vulnerable
-# rest == safe 
+# rest == safe
+# Versions reporting as "4.3.11" could be patched or not
 
 if [[ ( $sambamajor -eq 3 && $sambaminor -gt 4 ) || \
       ( $sambamajor -eq 4 && $sambaminor -lt 4 ) || \
@@ -46,28 +59,21 @@ if [[ ( $sambamajor -eq 3 && $sambaminor -gt 4 ) || \
   # vulnerable version 
   echo "The SAMBA version in this computer most likely (unless some specific security patches manually applied recently) is amongst the affected." 
   echo "If you are certain that no security patches and no SAMBA package updates have been performed recently, please do one of the following:"
-  echo " * [EASIEST] Upgrade SAMBA version. Centos, RHEL, Debian and Ubuntu have issued package updates to address the issue already."
-  echo " * [MEDIUM] disable named pipes in smb.conf"
-  echo " * [ADVANCED] patch a tarball of SAMBA sources, rebuild and reinstall"
-  echo " * [MOST INTELLIGENT] better remove SAMBA from the system and choose an alternative to share your files though the Internet ;)"
+  echo " a) [EASIEST] Upgrade SAMBA version. Centos, RHEL, Debian and Ubuntu have issued package updates to address the issue already."
+  echo " b) [MEDIUM] disable named pipes in smb.conf"
+  echo " c) [ADVANCED] patch a tarball of SAMBA sources, rebuild and reinstall"
+  echo " d) [MOST INTELLIGENT] better remove SAMBA from the system and choose an alternative to share your files though the Internet ;)"
   echo
-  echo "I can update the SAMBA package for you if you want and your distro es RHEL/CentOS/Debian/Ubuntu/Derivatives."
-  echo -n "Do it now? y/n:"
+  echo "If your GNU/Linux distro es RHEL/CentOS/Debian/Ubuntu/Derivative, I can do a) for you"
+  echo "(that is, updating the SAMBA package for you if you want)."
+  echo -n "Try to do it now? y/n:"
   read answer
   case answer in
   Y|y)
-    get_package_type
-    case OS in
-    "DEB")
-      apt-get update && apt-get install --only-upgrade samba && echo "Samba upgrade finished :)" || echo "Something went wrong! Could not automatically upgrade samba :("
-      ;;
-    "RPM")
-      yum update samba && echo "Samba upgrade finished :)" || echo "Something went wrong! Could not automatically upgrade samba :("
-      ;;
-    esac
+    perform_upgrade
     ;;
   *)
-    echo "OK. Not doing anything. Please take care..."
+    echo "OK. Not doing anything. Suit yourself..."
   ;;
   esac
 else
